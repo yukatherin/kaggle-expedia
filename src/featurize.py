bdc6a34410104798d6078a565ca9b9465b62410c
Katherine Yu <kyu@fanatics.com>
2016-06-04 11:26:59 -0700

    featurize.py

--- src/featurize.py
+++ src/featurize.py
@@ -11,9 +11,9 @@ import datetime
 def parsedate(x):
     try:
         parsed = pd.to_datetime(x)
-        if parsed < pd.to_datetime('2013'):
+        if parsed < pd.to_datetime(2013):
             return np.nan
-        elif str(parsed.date()) > '2017' or str(parsed.date()) < '2013' or len(str(parsed.date())) != 10:
+        elif str(parsed.date()) > 2017 or str(parsed.date()) < 2013 or len(str(parsed.date())) != 10:
             return np.nan
         return parsed.date()
     except:
@@ -22,26 +22,26 @@ def parsedate(x):
 
 def add_feat_transforms(df):
 
-    df['date_time_dt'] = df.date_time.map(parsedate)
-    df['srch_ci_dt'] = df.srch_ci.map(parsedate)
-    df['srch_co_dt'] = df.srch_co.map(parsedate)
+    df[date_time_dt] = df.date_time.map(parsedate)
+    df[srch_ci_dt] = df.srch_ci.map(parsedate)
+    df[srch_co_dt] = df.srch_co.map(parsedate)
 
-    df['book_dt_numeric'] = (df.date_time_dt - pd.to_datetime('2012-01-01').date()) / np.timedelta64(1, 'D')
-    df['planahead_window_days'] = (df.srch_ci_dt - df.date_time_dt) / np.timedelta64(1, 'D')
-    df['srch_staylength'] = (df['srch_co_dt'] - df['srch_ci_dt']) / np.timedelta64(1, 'D') + 1
-    df['srch_ci_dow'] = df.srch_ci_dt.map(lambda x: x.weekday() if type(x)==datetime.date else 0)
-    df['srch_co_dow'] = df.srch_co_dt.map(lambda x: x.weekday() if type(x)==datetime.date else 0)
-    df['srch_incl_weeknd'] = (df.srch_ci_dow + df.srch_staylength) >= 5
+    df[book_dt_numeric] = (df.date_time_dt - pd.to_datetime(2012-01-01).date()) / np.timedelta64(1, D)
+    df[planahead_window_days] = (df.srch_ci_dt - df.date_time_dt) / np.timedelta64(1, D)
+    df[srch_staylength] = (df[srch_co_dt] - df[srch_ci_dt]) / np.timedelta64(1, D) + 1
+    df[srch_ci_dow] = df.srch_ci_dt.map(lambda x: x.weekday() if type(x)==datetime.date else 0)
+    df[srch_co_dow] = df.srch_co_dt.map(lambda x: x.weekday() if type(x)==datetime.date else 0)
+    df[srch_incl_weeknd] = (df.srch_ci_dow + df.srch_staylength) >= 5
 
-    df = pd.concat((df, pd.get_dummies(df['srch_ci_dow'])))
+    df = pd.concat((df, pd.get_dummies(df[srch_ci_dow])))
     df.fillna(df.median(), inplace=True)
 
 
 
 # Read data
-train = pd.read_csv('data/train.csv', parse_dates=['date_time', 'srch_co', 'srch_ci'])
-test = pd.read_csv('data/test.csv', parse_dates=['date_time', 'srch_co', 'srch_ci'])
-dest = pd.read_csv('data/destinations.csv')
+train = pd.read_csv(data/train.csv, parse_dates=[date_time, srch_co, srch_ci])
+test = pd.read_csv(data/test.csv, parse_dates=[date_time, srch_co, srch_ci])
+dest = pd.read_csv(data/destinations.csv)
 
 
 # Add engineered features
@@ -50,44 +50,56 @@ add_feat_transforms(test)
 
 
 # Cluster counts by srch_dest_id
-counts_srch_destination_id = train.groupby('srch_destination_id').agg({'is_booking': sum, 'date_time': len})
-counts_srch_destination_id_hotel_cluster = train.groupby(['srch_destination_id', 'hotel_cluster']).agg({'is_booking': sum, 'date_time': len})
+counts_srch_destination_id = train.groupby(srch_destination_id).agg({is_booking: sum, date_time: len})
+counts_srch_destination_id_hotel_cluster = train.groupby([srch_destination_id, hotel_cluster]).agg({is_booking: sum, date_time: len})
 counts_srch_destination_id_hotel_cluster.reset_index(inplace=True)
-counts_srch_destination_id_hotel_cluster['tmp_total_ct'] = counts_srch_destination_id.ix[counts_srch_destination_id_hotel_cluster.srch_destination_id, 'date_time'].values
-counts_srch_destination_id_hotel_cluster['normalized_ct'] = counts_srch_destination_id_hotel_cluster['date_time'] / counts_srch_destination_id_hotel_cluster['tmp_total_ct']
+counts_srch_destination_id_hotel_cluster[tmp_total_ct] = counts_srch_destination_id.ix[counts_srch_destination_id_hotel_cluster.srch_destination_id, date_time].values
+counts_srch_destination_id_hotel_cluster[normalized_ct] = counts_srch_destination_id_hotel_cluster[date_time] / counts_srch_destination_id_hotel_cluster[tmp_total_ct]
 
-hotel_cluster_counts_by_srch_destination_id = pd.pivot_table(counts_srch_destination_id_hotel_cluster, values=['is_booking', 'date_time', 'normalized_ct'], index=['srch_destination_id'], columns=['hotel_cluster'], aggfunc=max)
+hotel_cluster_counts_by_srch_destination_id = pd.pivot_table(counts_srch_destination_id_hotel_cluster, values=[is_booking, date_time, normalized_ct], index=[srch_destination_id], columns=[hotel_cluster], aggfunc=max)
 hotel_cluster_counts_by_srch_destination_id.fillna(0, inplace=True)
 
 # Cluster counts by hotel_market
-counts_hotel_market = train.groupby('hotel_market').agg({'is_booking': sum, 'date_time': len})
-counts_hotel_market_hotel_cluster = train.groupby(['hotel_market', 'hotel_cluster']).agg({'is_booking': sum, 'date_time': len})
+counts_hotel_market = train.groupby(hotel_market).agg({is_booking: sum, date_time: len})
+counts_hotel_market_hotel_cluster = train.groupby([hotel_market, hotel_cluster]).agg({is_booking: sum, date_time: len})
 counts_hotel_market_hotel_cluster.reset_index(inplace=True)
-counts_hotel_market_hotel_cluster['tmp_total_ct'] = counts_hotel_market.ix[counts_hotel_market_hotel_cluster.hotel_market, 'date_time'].values
-counts_hotel_market_hotel_cluster['normalized_ct'] = counts_hotel_market_hotel_cluster['date_time'] / counts_hotel_market_hotel_cluster['tmp_total_ct']
+counts_hotel_market_hotel_cluster[tmp_total_ct] = counts_hotel_market.ix[counts_hotel_market_hotel_cluster.hotel_market, date_time].values
+counts_hotel_market_hotel_cluster[normalized_ct] = counts_hotel_market_hotel_cluster[date_time] / counts_hotel_market_hotel_cluster[tmp_total_ct]
 
-hotel_cluster_counts_by_hotel_market = pd.pivot_table(counts_hotel_market_hotel_cluster, values=['is_booking', 'date_time', 'normalized_ct'], index=['hotel_market'], columns=['hotel_cluster'], aggfunc=max)
+hotel_cluster_counts_by_hotel_market = pd.pivot_table(counts_hotel_market_hotel_cluster, values=[is_booking, date_time, normalized_ct], index=[hotel_market], columns=[hotel_cluster], aggfunc=max)
 hotel_cluster_counts_by_hotel_market.fillna(0, inplace=True)
 
-# Cluster counts by user_id
+# Cluster counts by site_name
 
+
+# Cluster counts by user_id
 
 
 # Join with cluster counts
 
 
+# Cluster counts by srch_dest_id, date
+
+
+
+# Bookings 7d, 30d
+
+
+
+
+
 
 # PCA
 # from sklearn.decomposition import PCA
-# dest.set_index('hotel_market', inplace=True)
+# dest.set_index(hotel_market, inplace=True)
 # pca = PCA(n_components=3)
 # dest_pca = pca.fit_transform(dest)
 # dest_pca = pd.DataFrame(dest_pca)
-# dest_pca['srch_destination_id'] = dest.index
+# dest_pca[srch_destination_id] = dest.index
 
 # Join with destinations latent variables
-train_dest = pd.merge(train, dest_pca, on='srch_destination_id')
-test_dest = pd.merge(test, dest_pca, on='srch_destination_id')
+train_dest = pd.merge(train, dest_pca, on=srch_destination_id)
+test_dest = pd.merge(test, dest_pca, on=srch_destination_id)
 
 
 # Validation sample
